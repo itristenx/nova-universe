8514324a8a357bd8bc5779ac33d1da539482d02c
Tristen <69410611+itristenx@users.noreply.github.com>
2025-07-04 19:48:03 -0400

    chore: detect mac arch in installer

--- installers/make-installer.sh
+++ installers/make-installer.sh
@@ -6,16 +6,21 @@ cd "$SCRIPT_DIR/.."
 APP_DIR="cueit-macos"
 VERSION="${1:-1.0.0}"
 
+arch=$(uname -m)
+if [[ "$arch" == "x86_64" ]]; then
+  arch="x64"
+fi
+
 npm --prefix "$APP_DIR" install
 npx --prefix "$APP_DIR" electron-packager "$APP_DIR" CueIT \
-  --platform=darwin --out "$APP_DIR/dist" --overwrite
+  --platform=darwin --arch="$arch" --out "$APP_DIR/dist" --overwrite
 
-APP_PATH="$APP_DIR/dist/CueIT-darwin-x64/CueIT.app"
+APP_PATH="$APP_DIR/dist/CueIT-darwin-$arch/CueIT.app"
 
-mkdir -p "$APP_DIR/dist/CueIT-darwin-x64/resources"
-cp -R cueit-api cueit-admin cueit-activate cueit-slack installers/start-all.sh "$APP_DIR/dist/CueIT-darwin-x64/resources/"
+mkdir -p "$APP_DIR/dist/CueIT-darwin-$arch/resources"
+cp -R cueit-api cueit-admin cueit-activate cueit-slack installers/start-all.sh "$APP_DIR/dist/CueIT-darwin-$arch/resources/"
 if [[ -f cert.pem && -f key.pem ]]; then
-  cp cert.pem key.pem "$APP_DIR/dist/CueIT-darwin-x64/resources/"
+  cp cert.pem key.pem "$APP_DIR/dist/CueIT-darwin-$arch/resources/"
 fi
 
 pkgbuild --root "$APP_PATH" --identifier com.cueit.launcher \
