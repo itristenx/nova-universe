734d4cb4443d01efa1fd8d3a96ca3f215d3fe89a
Tristen <69410611+itristenx@users.noreply.github.com>
2025-07-28 15:16:39 -0400

    fix: use graphql-tag for gql

--- apps/api/graphql.js
+++ apps/api/graphql.js
@@ -1,6 +1,6 @@
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
-import { gql } from '@apollo/server';
+import gql from 'graphql-tag';
 import db from './db.js';
 import { verify } from './jwt.js';
 
