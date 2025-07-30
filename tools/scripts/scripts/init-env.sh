5c46d0aefac62538f2fb4d3654ba2041798c291b
Tristen <69410611+itristenx@users.noreply.github.com>
2025-07-30 10:47:22 -0400

    fix: update setup paths

--- tools/scripts/scripts/init-env.sh
+++ tools/scripts/scripts/init-env.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 set -e
 
-apps=(nova-api nova-core nova-comms)
+apps=("apps/api" "apps/core/nova-core" "apps/comms/nova-comms")
 
 for app in "${apps[@]}"; do
   example="$app/.env.example"
   env="$app/.env"
+  if [ -f "$env" ]; then
+    continue
+  fi
+
   if [ -f "$example" ]; then
-    if [ ! -f "$env" ]; then
-      cp "$example" "$env"
-      echo "Created $env. Remember to edit the values." >&2
-    fi
+    cp "$example" "$env"
+    echo "Created $env. Remember to edit the values." >&2
+  elif [ -f ".env.example" ]; then
+    cp .env.example "$env"
+    echo "Copied root .env.example to $env. Edit the values." >&2
   fi
 done
